63d34bf18c3cb1f1857c864f279abf2f91b6c5d7
Vishad-byte <srivastavavishad0212@gmail.com>
2025-05-20 21:39:01 +0530

    minor comment

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,6 +18,6 @@ const uploadOnCloudinary = async (localFilePath) => {
         return response;
         
     } catch (error) {
-        fs.unlinkSync(localFilePath)   // remove the locally saved temporary file as the upload op
+        fs.unlinkSync(localFilePath)   // remove the locally saved temporary file as the upload 
     }
 }
