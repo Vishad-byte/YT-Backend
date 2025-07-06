1a61a17f6d2e99d79fad9579742f95d02dfe9f7a
Vishad-byte <srivastavavishad0212@gmail.com>
2025-07-06 20:50:47 +0530

    Completed tweet controller, routes and tested on postman

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,19 +6,141 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+    const { content } = req.body;
+    if(!content || content.trim() === "") {
+        throw new ApiError(400, "Content is required");
+    }
+    const tweet = await Tweet.create({
+        content: content.trim(),
+        owner: req.user._id
+    });
+    if(!tweet) {
+        throw new ApiError(500, "Failed to create tweet");
+    }
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201, tweet, "Tweet created successfully")
+    );
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+
+    const{userId} = req.params;
+    if(!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user ID");
+    }
+
+    const allTweets = await Tweet.aggregate([
+        {
+            $match:
+            {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField:"_id",
+                as:"owner"
+
+            }
+        },
+        {
+            $unwind: "$owner"
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField:"_id",
+                foreignField:"tweet",
+                as: "likes"
+            }
+        },
+        {
+            $addFields: {
+                likeCount: {
+                    $size: "$likes"
+                }
+            }
+        },
+        {
+            $project: {
+                content: 1,
+                createdAt: 1,
+                updatedAt: 1,
+                "owner.username": 1,
+                "owner.fullName": 1,
+                "owner._id": 1,
+                likeCount: 1,
+            }
+        },
+        {
+            $limit: 10
+        } 
+
+    ])
+
+    if(!allTweets || allTweets.length === 0) {
+        throw new ApiError(404, "No tweets found for this user");
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, allTweets, "User tweets fetched successfully")
+    )
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const { tweetId } = req.params;
+    const { content } = req.body;
+
+    if(!tweetId || !isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID");
+    }
+
+    if(!content || content.trim() === "") {
+        throw new ApiError(400, "Content is required");
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+    tweetId,
+        {
+            $set: {
+                content: content.trim()
+            }
+        }, 
+        { new: true }
+    );
+
+    if(!updatedTweet) {
+        throw new ApiError(500, "Tweet failed to update");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, updatedTweet, "Tweet updated successfully")
+    ) 
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const { tweetId } = req.params;
+    if(!tweetId || !isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID");
+    }
+
+    const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
+    if(!deletedTweet) {
+        throw new ApiError(500, "Tweet failed to delete");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, deletedTweet, "Tweet deleted successfully")
+    )
 })
 
 export {
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -10,8 +10,9 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/").post(createTweet);
-router.route("/user/:userId").get(getUserTweets);
-router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+router.route("/create-tweet").post(createTweet);
+router.route("/get-user-tweets/:userId").get(getUserTweets);
+router.route("/update-tweet/:tweetId").patch(updateTweet);
+router.route("/delete-tweet/:tweetId").delete(deleteTweet);
 
 export default router
